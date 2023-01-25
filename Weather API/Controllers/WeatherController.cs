a7cb6610201f3b67c6e43b95ddb47228f0f5cf0d
IKECHUKWU <sundayjabikem@gmail.com>
2023-01-25 22:50:38 +0100

    completed Weather Controller

--- Weather API/Controllers/WeatherController.cs
+++ Weather API/Controllers/WeatherController.cs
@@ -1,5 +1,7 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using Weather_API.DTO;
+using Weather_API.Models;
 using Weather_API.Repositories.Interfaces;
 
 namespace Weather_API.Controllers
@@ -10,13 +12,44 @@ namespace Weather_API.Controllers
     {
         private readonly IWeatherRepository _weatherRepository;
         private readonly ILogger<WeatherController> _logger;
-        private readonly IMapper _mapper;
+        
 
-        public WeatherController(IWeatherRepository weatherRepository, ILogger<WeatherController> logger, IMapper mapper)
+        public WeatherController(IWeatherRepository weatherRepository, ILogger<WeatherController> logger)
         {
             _weatherRepository = weatherRepository;
             _logger = logger;
-            _mapper = mapper;
+           
         }
+
+        [HttpGet("GetByCountry")]
+        public async Task<ActionResult<Weather>> GetByCOuntry(string country) 
+        {
+            var result = await _weatherRepository.GetWeatherByCountry(country);
+            return Ok(result);
+        }
+
+        [HttpGet("GetByCity")]
+        public async Task<ActionResult<Weather>> GetByCity(string city)
+        {
+            var result = await _weatherRepository.GetWeatherByCity(city);
+            return Ok(result);
+        }
+
+        [HttpPost("CreateWeather")]
+        public async Task<ActionResult> CreateWeather([FromBody] WeatherDTO weather)
+        {
+            var create = await _weatherRepository.CreateWeather(weather);
+            return Ok(create);
+        }
+
+        [HttpPut("UpdateWeather")]
+        public async Task<ActionResult> UpdateWeather([FromBody] WeatherDTO weather)
+        {
+            var create = await _weatherRepository.UpdateWeatherInfo(weather);
+            return Ok(create);
+        }
+
+
+
     }
 }
